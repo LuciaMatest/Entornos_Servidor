ec0292868ce44c06a23a227aaf4240586f7f330f
LuciaMatest <114143383+LuciaMatest@users.noreply.github.com>
2022-10-07 11:05:57 +0200

    Add files via upload

---
+++ PRACTICAS/PR02_LuciaMateos/holaMundoIdioma.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Idiomas</title>
+</head>
+<body>
+    <?php
+        $es="Hola Mundo";
+        $en="Hello worlds";
+        $nr="Hei Verden";
+        $it="Ciao mondo";
+        $de="Hallo welt";
+        $idioma=$_GET['pais'];
+        echo "${$idioma}";
+    ?>
+</body>
+</html>
