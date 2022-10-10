98abd62ffe885f430310e21113575c5806fbf936
Lucia <lucia.matest@educa.jcyl.es>
2022-10-10 18:30:45 +0000

    1

--- Tema3/piramide.php
+++ Tema3/piramide.php
@@ -1,11 +1,11 @@
 <?
 $a = 5;
 for ($i=1; $i <= $a; $i++) { 
-    for ($j=1; $j <= ($a - $i); $j++) { 
-        echo "&nbsp";
+    for ($j=1; $j <= $a - $i; $j++) { 
+        echo '&nbsp;&nbsp;';
     }
-    for ($h=1; $h <= ($a*2) - 1; $h++) { 
-        echo "*";
+    for ($h=1; $h <= 2*$i - 1; $h++) { 
+        echo '*';
     }
-    echo "<br>";
+    echo '<br>';
 }
