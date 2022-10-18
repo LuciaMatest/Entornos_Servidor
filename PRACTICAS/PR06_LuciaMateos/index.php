dcea6af5df0e836370bfda66d699fe28864a060c
Lucia <lucia.matest@educa.jcyl.es>
2022-10-18 09:10:01 +0000

    1

--- PRACTICAS/PR06_LuciaMateos/index.php
+++ PRACTICAS/PR06_LuciaMateos/index.php
@@ -67,25 +67,27 @@
             echo("<tr>");
             echo("<th>EQUIPOS</th>");
             //primera fila
+            $equipos = array();
             foreach ($liga as $key => $value) {
                 echo("<th>&nbsp;" .$key. "&nbsp;</th>");
+                array_push($equipos, $key);
             }
             echo("</tr>");
             //columna
             $filas = 0;
             $colum = 0;
            
-            foreach ($liga as $equipo => $valor) {
+            foreach ($liga as $nombre => $valor) {
                 echo("<tr>");
-                echo("<th>&nbsp;" .$equipo. "&nbsp;</th>");
-                foreach ($valor as $puntos => $equipo) {
+                echo("<th>&nbsp;" .$nombre. "&nbsp;</th>");
+                foreach ($valor as $puntos => $nombre) {
                     if ($filas==$colum) {
                         echo("<td></td>");
                     }
                     echo("<td>");
                     $colum++;
                     //puntos
-                    foreach ($equipo as $key => $valor) {
+                    foreach ($nombre as $key => $valor) {
                         if($key == "Resultado"){
                             echo("<p>&nbsp;" .$valor. "&nbsp;</p>");
                         }
@@ -99,6 +101,71 @@
                 $colum = 0;
                 $filas++;
             }
+        ?>
+    </table>
+    <br>
+    <table>
+    <?php
+        $liga = array(
+            "Zamora" =>  array(
+                "Salamanca" => array(
+                    "Resultado" => "3-2", "Roja" => 1, "Amarilla" => 0, "Penalti" => 0
+                ),
+                "Avila" => array(
+                    "Resultado" => "4-1", "Roja" => 0, "Amarilla" => 0, "Penalti" => 0
+                ),
+                "Valladolid" => array(
+                    "Resultado" => "1-2", "Roja" => 1, "Amarilla" => 1, "Penalti" => 1
+                )
+            ),
+            "Salamanca" =>  array(
+                "Zamora" => array(
+                    "Resultado" => "3-2", "Roja" => 1, "Amarilla" => 0, "Penalti" => 0
+                ),
+                "Avila" => array(
+                    "Resultado" => "4-1", "Roja" => 0, "Amarilla" => 0, "Penalti" => 0
+                ),
+                "Valladolid" => array(
+                    "Resultado" => "1-2", "Roja" => 1, "Amarilla" => 2, "Penalti" => 1
+                )
+            ),
+            "Avila" =>  array(
+                "Zamora" => array(
+                    "Resultado" => "3-2", "Roja" => 1, "Amarilla" => 0, "Penalti" => 2
+                ),
+                "Salamanca" => array(
+                    "Resultado" => "1-3", "Roja" => 1, "Amarilla" => 3, "Penalti" => 0
+                ),
+                "Valladolid" => array(
+                    "Resultado" => "1-3", "Roja" => 1, "Amarilla" => 0, "Penalti" => 1
+                )
+            ),
+            "Valladolid" =>  array(
+                "Zamora" => array(
+                    "Resultado" => "3-2", "Roja" => 1, "Amarilla" => 0, "Penalti" => 0
+                ),
+                "Salamanca" => array(
+                    "Resultado" => "3-1", "Roja" => 0, "Amarilla" => 0, "Penalti" => 0
+                ),
+                "Avila" => array(
+                    "Resultado" => "1-2", "Roja" => 1, "Amarilla" => 1, "Penalti" => 2
+                )
+            ),
+        );
+        ?>
+        <?php
+            $marcador = array();
+            foreach ($equipos as $key => $value) {
+                $marcador [$value] = array("Puntos" => 0, "Goles a favor" => 0, "Goles en contra" => 0);
+            }
+
+            echo("<tr>");
+            echo("<th>EQUIPOS</th>");
+            //primera fila
+            foreach ($equipos as $key => $value) {
+                echo("<th>&nbsp;" .$key. "&nbsp;</th>");
+            }
+            echo("</tr>");
         ?>
     </table>
 </body>
