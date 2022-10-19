cb2b0b150ec878fb334441e8672aa42f367abab3
Lucia <lucia.matest@educa.jcyl.es>
2022-10-19 19:00:58 +0000

    1

--- PRACTICAS/PR06_LuciaMateos/index.php
+++ PRACTICAS/PR06_LuciaMateos/index.php
@@ -105,54 +105,6 @@
     </table>
     <br>
     <table>
-    <?php
-        $liga = array(
-            "Zamora" =>  array(
-                "Salamanca" => array(
-                    "Resultado" => "3-2", "Roja" => 1, "Amarilla" => 0, "Penalti" => 0
-                ),
-                "Avila" => array(
-                    "Resultado" => "4-1", "Roja" => 0, "Amarilla" => 0, "Penalti" => 0
-                ),
-                "Valladolid" => array(
-                    "Resultado" => "1-2", "Roja" => 1, "Amarilla" => 1, "Penalti" => 1
-                )
-            ),
-            "Salamanca" =>  array(
-                "Zamora" => array(
-                    "Resultado" => "3-2", "Roja" => 1, "Amarilla" => 0, "Penalti" => 0
-                ),
-                "Avila" => array(
-                    "Resultado" => "4-1", "Roja" => 0, "Amarilla" => 0, "Penalti" => 0
-                ),
-                "Valladolid" => array(
-                    "Resultado" => "1-2", "Roja" => 1, "Amarilla" => 2, "Penalti" => 1
-                )
-            ),
-            "Avila" =>  array(
-                "Zamora" => array(
-                    "Resultado" => "3-2", "Roja" => 1, "Amarilla" => 0, "Penalti" => 2
-                ),
-                "Salamanca" => array(
-                    "Resultado" => "1-3", "Roja" => 1, "Amarilla" => 3, "Penalti" => 0
-                ),
-                "Valladolid" => array(
-                    "Resultado" => "1-3", "Roja" => 1, "Amarilla" => 0, "Penalti" => 1
-                )
-            ),
-            "Valladolid" =>  array(
-                "Zamora" => array(
-                    "Resultado" => "3-2", "Roja" => 1, "Amarilla" => 0, "Penalti" => 0
-                ),
-                "Salamanca" => array(
-                    "Resultado" => "3-1", "Roja" => 0, "Amarilla" => 0, "Penalti" => 0
-                ),
-                "Avila" => array(
-                    "Resultado" => "1-2", "Roja" => 1, "Amarilla" => 1, "Penalti" => 2
-                )
-            ),
-        );
-        ?>
         <?php
             $marcador = array();
             foreach ($equipos as $key => $value) {
@@ -160,13 +112,38 @@
             }
 
             echo("<tr>");
-            echo("<th>EQUIPOS</th>");
-            //primera fila
-            foreach ($equipos as $key => $value) {
-                echo("<th>&nbsp;" .$key. "&nbsp;</th>");
+            echo ("<th>Equipos</th>");
+            echo ("<th>Puntos</th>");
+            echo ("<th>Goles a favor</th>");
+            echo ("<th>Goles en contra</th></tr>");
+            foreach ($liga as $nombre => $partidos) {
+                foreach ($partidos as $valor => $resultado) {               
+                    list($gf,$gc)=explode("-", $resultado["Resultado"]);
+                    $marcador[$nombre]["Goles a favor"]+=$gf;
+                    $marcador[$valor]["Goles en contra"]+=$gf;
+                    $marcador[$nombre]["Goles en contra"]+=$gc;
+                    $marcador[$valor]["Goles a favor"]+=$gc;
+                    if ($gf>$gc){
+                        $marcador[$nombre]["Puntos"]+=3;
+                    }elseif ($gf==$gc) {
+                        $marcador[$nombre]["Puntos"]+=1;                
+                        $marcador[$valor]["Puntos"]+=1;                
+                    }else {
+                        $marcador[$valor]["Puntos"]+=3;
+                        
+                    }
+                }
             }
-            echo("</tr>");
+    
+            foreach ($marcador as $local => $resultado) {
+                echo "<tr><td>" . $local . "</td>";
+                foreach ($resultado as $valor) {
+                  echo "<td>". $valor . "</td>" ; 
+                }
+                echo "</tr>";
+            }
         ?>
     </table>
 </body>
 </html>
+
