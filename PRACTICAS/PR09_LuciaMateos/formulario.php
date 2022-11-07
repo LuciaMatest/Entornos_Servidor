4e3cc3d55279bcf2e02cccff3ca166e9cca87006
Lucia <lucia.matest@educa.jcyl.es>
2022-11-07 20:24:10 +0000

    1

---
+++ PRACTICAS/PR09_LuciaMateos/formulario.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PR09</title>
+</head>
+<body>
+    <?php
+        echo '<link rel="stylesheet" href="../../style.css">';
+    ?>
+    <header>
+        <h1>PR03</h1>
+    </header>
+    <main>
+    <ul class="menú">
+      <li><a href="#">Apuntes</a></li>
+    </ul>
+    <ul class="menú">
+      <li><a href="../index.html">Volver</a></li>
+    </ul>
+  </main>
+    <footer>
+        <p>2ºDAW - Lucia Mateos Esteban</p>
+    </footer>
+</body>
+</html>
