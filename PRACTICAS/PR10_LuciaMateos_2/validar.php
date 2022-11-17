7052d1425064e76dbe7c85aa22b769f306f43946
Lucia <lucia.matest@educa.jcyl.es>
2022-11-17 11:39:27 +0000

    1

---
+++ PRACTICAS/PR10_LuciaMateos_2/validar.php
@@ -0,0 +1,15 @@
+<?php
+function enviado(){
+    if (isset($_REQUEST['editar']) || isset($_REQUEST['leer'])) {
+        return true;
+    }
+    return false;
+}
+
+function existe($nombre){
+    if (isset($_REQUEST[$nombre])) {
+        return true;
+    }
+    return false;
+}
+?>
