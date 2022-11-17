4faaab4ba09a8c7897250c880dfe6e5e803fff24
Lucia <lucia.matest@educa.jcyl.es>
2022-11-17 11:59:26 +0000

    1

---
+++ PRACTICAS/PR10_LuciaMateos_2/tablaFichero.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./style.css">
+    <title>tabla notas</title>
+</head>
+
+<body>
+    <header>
+        <h1>PR10</h1>
+    </header>
+    <main>
+        <ul class="menú">
+            <li><a href="#">Notas</a></li>
+        </ul>
+
+        <table style="width:100%">
+            <tr>
+                <th>Alumno</th>
+                <th>Nota 1</th>
+                <th>Nota 2</th>
+                <th>Nota 3</th>
+                <th>Editar</th>
+            </tr>
+            <tr>
+                <td>Alfreds Futterkiste</td>
+                <td>Maria Anders</td>
+                <td>Germany</td>
+            </tr>
+            <tr>
+                <td>Centro comercial Moctezuma</td>
+                <td>Francisco Chang</td>
+                <td>Mexico</td>
+            </tr>
+        </table>
+
+
+        <ul class="menú">
+            <!-- Codigos PHP -->
+            <li><a href="verCodigo.php?fichero=eligeFichero.php">Código principal</a></li>
+            <li><a href="verCodigo.php?fichero=validar.php">Código funciones</a></li>
+
+
+            <li><a href="../../index.html">Volver</a></li>
+        </ul>
+    </main>
+</body>
+
+</html>
