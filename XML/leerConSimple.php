5bd486ea687a31910de805e1fac773c69a7ea24d
Lucia <lucia.matest@educa.jcyl.es>
2022-11-22 10:04:55 +0000

    1

---
+++ XML/leerConSimple.php
@@ -0,0 +1,13 @@
+<?
+$mundial = simplexml_load_file('mundial.xml');
+
+foreach ($mundial as $equipo) {
+    echo '<br>Equipo: ' . $equipo ->children()[0];
+    echo ' y entrenador: ' . $equipo ->children()[1];
+}
+
+$equipo = $mundial->addChild('Equipo');
+$equipo ->addChild('Nombre', 'Italia');
+$equipo ->addChild('Entrenador', 'Alexandro');
+
+$mundial->asXML('mundial2.xml');
