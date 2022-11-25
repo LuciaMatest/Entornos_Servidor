7e7b2422f80ea44e0e8428b91262bee04f62c0cd
Lucia <lucia.matest@educa.jcyl.es>
2022-11-25 12:22:29 +0000

    1

--- EXAMEN_LUCIA/Examen_2/Examen2.php
+++ EXAMEN_LUCIA/Examen_2/Examen2.php
@@ -8,6 +8,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <title>Examen - Lucia Mateos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
@@ -26,7 +27,11 @@
         "2DAM" => array("DI", "SGE", "ACDA", "EIE", "PSP"),
         "2DAW" => array("DWES", "DWEC", "DIW", "EIE")
     );
-
+    ?>
+    <?php
+        if (primeraComprobación()){
+            mostrar();
+        }else{
     ?>
     <form action="./Examen2.php" method="post">
         <label for="nombre">Nombre y apellidos:</label> <input type="text" name="nombre" id="nombre" 
@@ -98,12 +103,12 @@
                 $_REQUEST['curso'];
             }           
         ?>">
-        
+    
         <input type="submit" name="Enviado" value="Enviar">
     </form>
-
-
-
+    <?php
+        }
+    ?>
 </body>
 
 </html>
