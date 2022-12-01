c7cefe2a54837773019e39d2197f81d565bc90a8
Lucia <lucia.matest@educa.jcyl.es>
2022-12-01 12:13:26 +0000

    1

--- Tema4/mysqli.php
+++ Tema4/mysqli.php
@@ -4,7 +4,8 @@ try{
     $conexion= mysqli_connect(HOST,USER,PASS);
 }catch(Exception $ex){
     echo $ex;
-    if($ex->code==2002){
+    //el codigo de error con Depurar
+    if($ex->code===2002){
         echo " Error 2002 No se puede conectar a la base de datos";
     }else if($ex->code==1045){
         echo " Error 1045 acceso denegado para usuario";
