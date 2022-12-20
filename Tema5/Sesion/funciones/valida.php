8aca3b802deb98ced601ac91ec903cb061d1ec18
Lucia <lucia.matest@educa.jcyl.es>
2022-12-20 09:29:36 +0000

    1

--- Tema5/Sesion/funciones/valida.php
+++ Tema5/Sesion/funciones/valida.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require './BD.php';
 //Sacamos usuario y contraseña del formulario donde se van a introducir los datos
 $user = $_REQUEST['user'];
@@ -6,6 +7,10 @@ $pass = $_REQUEST['pass'];
 
 if (empty($user)) {
     $_SESSION['error'] = 'Debe rellenar el nombre';
+    header('Location : ../login.php');
+    exit;
+}elseif (empty($pass)) {
+    $_SESSION['error'] = 'Debe rellenar el contraseña';
     header('Location : ../login.php');
     exit;
 } else {
