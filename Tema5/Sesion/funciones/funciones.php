81a4a569404fcf225cd0beb8e4d4c7872a6913a9
Lucia <lucia.matest@educa.jcyl.es>
2022-12-20 08:27:47 +0000

    1

---
+++ Tema5/Sesion/funciones/funciones.php
@@ -0,0 +1,18 @@
+<?php
+
+function estaValidado(){
+    if (isset($_SESSION['validado'])) {
+        return true;
+    }
+    return false;
+}
+
+function esAdmin(){
+    if (isset($_SESSION['perfil'])) {
+        if ($_SESSION['perfil'] == 'ADM01')
+            return true;
+    }
+    return false;
+}
+
+?>
--- Tema5/Sesion/paginas/admin.php
+++ Tema5/Sesion/paginas/admin.php
@@ -1,4 +1,10 @@
-<h1>Administrador</h1>
 <?php
+    session_start();
+    require '../funciones/funciones.php';
 
+    if(!estaValidado() || !esAdmin()){
+        header('Location: ../login.php');
+        exit;
+    }
 ?>
+<h1>Administrador</h1>
