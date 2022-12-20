0a1acd64ca255bac6cab2c8a9eb9675ae1a41752
Lucia <lucia.matest@educa.jcyl.es>
2022-12-20 09:17:41 +0000

    1

--- Tema5/Sesion/funciones/valida.php
+++ Tema5/Sesion/funciones/valida.php
@@ -4,16 +4,23 @@ require './BD.php';
 $user = $_REQUEST['user'];
 $pass = $_REQUEST['pass'];
 
-if (validarUser($user, $pass)) {
-    if ($_SESSION['perfil'] == 'ADM01') {
-        header('Location: ../paginas/admin.php');
-        exit;
+if (empty($user)) {
+    $_SESSION['error'] = 'Debe rellenar el nombre';
+    header('Location : ../login.php');
+    exit;
+} else {
+    if (validarUser($user, $pass)) {
+        if ($_SESSION['perfil'] == 'ADM01') {
+            header('Location: ../paginas/admin.php');
+            exit;
+        } else {
+            header('Location: ../paginas/home.php');
+            exit;
+        }
     } else {
-        header('Location: ../paginas/home.php');
+        $_SESSION['error'] = 'No existe el usuario';
+        header('Location: ../login.php');
         exit;
     }
-} else {
-    header('Location: ../login.php');
-    exit;
 }
 ?>
