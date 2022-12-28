292e9d950148bd0ed625a9cc3bd8a53de0b7b46f
Lucia <lucia.matest@educa.jcyl.es>
2022-12-28 18:18:53 +0000

    1

--- PRACTICAS/PR14_LuciaMateos/index.php
+++ PRACTICAS/PR14_LuciaMateos/index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="./Estilo/style.css">
-    <title>Index</title>
+    <title>Index - PR13</title>
 </head>
 
 <body>
