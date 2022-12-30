5b7e1c55b490ef27b0d87f90ea12b8e0837e58c2
Lucia <lucia.matest@educa.jcyl.es>
2022-12-30 14:56:21 +0000

    1

---
+++ PRACTICAS/PR15_LuciaMateos/BBDD/peluqueria.sql
@@ -0,0 +1,63 @@
+create database peluqueria;
+use peluqueria;
+
+CREATE TABLE roles (
+    codigo CHAR(5) PRIMARY KEY,
+	descripcion VARCHAR(30)
+) engine=innodb;
+
+create table usuarios (
+	usuario CHAR(20) PRIMARY KEY,
+	clave CHAR(40) NOT NULL,
+	nombre VARCHAR(75) NOT NULL,
+	correo VARCHAR(75) NOT NULL,
+    fecha DATE NOT NULL,
+	roles CHAR(5) NOT NULL,
+	index (roles),
+	foreign key (roles) references roles (codigo)
+) engine=innodb;
+
+CREATE TABLE productos (
+    cod_producto INT PRIMARY KEY,
+    imagen CHAR(30) NOT NULL,
+    nombre CHAR(30) NOT NULL,
+    descripcion CHAR(140) NOT NULL,
+    precio NUMERIC(5,2) NOT NULL,
+    stock INT(3) NOT NULL,
+) engine=innodb;
+
+insert into roles (codigo, descripcion) values ('ADM01','Administrador');
+insert into roles (codigo, descripcion) values ('M0001','Moderador');
+insert into roles (codigo, descripcion) values ('U0001','Usuario');
+
+insert into productos (cod_producto, imagen, nombre, descripcion, precio, stock) values ('1117','./imagen/1.png','Tahe Botanic','Mascarilla Aceite de Árgan, Aceite de Amapola y Aceite de Palo de Rosa 700 ml','21.80','7');
+insert into productos (cod_producto, imagen, nombre, descripcion, precio, stock) values ('2596','./imagen/2.png','Tahe Natural Hair','Champu Dermorelax Essence 1000 ml','19.95','10');
+insert into productos (cod_producto, imagen, nombre, descripcion, precio, stock) values ('3030','./imagen/3.png','Postquam','Champú de hierbas 1000 ml','6.90','4');
+insert into productos (cod_producto, imagen, nombre, descripcion, precio, stock) values ('3152','./imagen/4.png','Crawford','Champú nutrición 1000 ml','10.61','12');
+insert into productos (cod_producto, imagen, nombre, descripcion, precio, stock) values ('3319','./imagen/5.png','Revlon','Champú Micellar Equave hidratante con queratina 250 ml','13.81','6');
+insert into productos (cod_producto, imagen, nombre, descripcion, precio, stock) values ('4497','./imagen/6.png','Postquam','Emulsión Suavizante Cabellos Teñidos 1000 ml','8.90','2');
+
+create table ventas (
+    id_ventas INT PRIMARY KEY AUTO_INCREMENT,
+    usuario_ventas CHAR(30) NOT NULL,
+    fecha_compra DATE NOT NULL,
+    cod_producto INT NOT NULL,
+    cantidad INT NOT NULL,
+    precio_total NUMERIC(10,2) NOT NULL,
+	index(usuario_ventas),
+    index(cod_Producto),
+	foreign key (usuario_ventas) references usuarios (usuario),
+	foreign key (cod_producto) references productos (cod_producto)
+) engine=innodb;
+
+create table albaran (
+    id_albaran INT PRIMARY KEY AUTO_INCREMENT,
+    fecha_albaran DATE NOT NULL,
+    cod_producto INT NOT NULL,
+    cantidad INT NOT NULL,
+    usuario_albaran CHAR(30) NOT NULL,
+    index(usuario_albaran),
+    index(cod_Producto),
+    foreign key (usuario_albaran) references usuarios (usuario),
+	foreign key (cod_producto) references productos (cod_producto)
+) engine=innodb;
---
+++ PRACTICAS/PR15_LuciaMateos/Funciones/funcionesBD.php
@@ -0,0 +1,116 @@
+<?php
+function existe($nombre){
+    if (isset($_REQUEST[$nombre]))
+        return true;
+    return false;
+}
+
+function vacio($nombre){
+    if (empty($_REQUEST[$nombre])) {
+        return true;
+    }
+    return false;
+}
+
+function enviado(){
+    if (isset($_REQUEST['enviar'])) {
+        return true;
+    }
+    return false;
+}
+
+function crearBBDD(){
+    if (isset($_REQUEST['crear']))
+    return true;
+    return false;
+}
+
+function usarBBDD(){
+    return file_get_contents('./BBDD/musica.sql');
+}
+
+//Patrones
+function patronID(){
+    $patron = '/\d{1,}$/';
+    if (preg_match($patron, $_REQUEST['id'])==1) {
+        return true;
+    }
+    return false;
+}
+
+function patronFecha(){
+    $patron = '/^(\d{4})\-(0[1-9]|1[0-2])\-([0-2][0-9]|3[0-1])$/';
+    if(preg_match($patron, $_REQUEST['fecha'])==1){
+        return true;
+    }
+    return false;
+}
+
+function patronDuracion(){
+    $patron = '/^(\d{1,3})\.(\d{2})$/';
+    if (preg_match($patron, $_REQUEST['duracion'])==1) {
+        return true;
+    }
+    return false;
+}
+
+function modificarDatos(){
+    try {
+        $conexion = new PDO('mysql:host='.$_SERVER['SERVER_ADDR'].';dbname='.BBDD, USER, PASS);
+        $actualiza = "update canciones set id='" .$_REQUEST['id']. "',fecha='" .$_REQUEST['fecha']. "',cancion='" .$_REQUEST['cancion']. "',duracion='" .$_REQUEST['duracion']. "' where id='" . $_REQUEST['clave'] . "';" ;
+        $conexion->exec($actualiza);
+    } catch (Exception $ex) {
+        if ($ex->getCode() == 2002) {
+            echo '<span style="color:brown"> Fallo de conexión </span>';
+        }
+        if ($ex->getCode() == 1049) {
+            echo '<span style="color:brown"> Base de datos desconocida </span>';
+        }
+        if ($ex->getCode() == 1045) {
+            echo '<span style="color:brown"> Datos incorrectos </span>';
+        }
+    }finally{
+        unset($conexion);
+    }
+}
+
+function insetarDatos(){
+    try {
+        $conexion = new PDO('mysql:host='.$_SERVER['SERVER_ADDR'].';dbname='.BBDD, USER, PASS);
+        $inserta = "insert into canciones values ('" .$_REQUEST['id']. "','" .$_REQUEST['fecha']. "','" .$_REQUEST['cancion']. "','" .$_REQUEST['duracion']. "');" ;
+        $conexion->exec($inserta);
+    } catch (Exception $ex) {
+        if ($ex->getCode() == 2002) {
+            echo '<span style="color:brown"> Fallo de conexión </span>';
+        }
+        if ($ex->getCode() == 1049) {
+            echo '<span style="color:brown"> Base de datos desconocida </span>';
+        }
+        if ($ex->getCode() == 1045) {
+            echo '<span style="color:brown"> Datos incorrectos </span>';
+        }
+    }finally{
+        unset($conexion);
+    }
+}
+
+function eliminarDatos(){
+    try {
+        $conexion = new PDO('mysql:host='.$_SERVER['SERVER_ADDR'].';dbname='.BBDD, USER, PASS);
+        $elimina = "delete from canciones where id='" .$_REQUEST['clave']. "';" ;
+        $conexion->exec($elimina);
+    } catch (Exception $ex) {
+        if ($ex->getCode() == 2002) {
+            echo '<span style="color:brown"> Fallo de conexión </span>';
+        }
+        if ($ex->getCode() == 1049) {
+            echo '<span style="color:brown"> Base de datos desconocida </span>';
+        }
+        if ($ex->getCode() == 1045) {
+            echo '<span style="color:brown"> Datos incorrectos </span>';
+        }
+    }finally{
+        unset($conexion);
+    }
+}
+?>
