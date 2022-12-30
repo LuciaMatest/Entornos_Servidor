642aecafcb863a512efab013ce41a8fcd91a46f4
Lucia <lucia.matest@educa.jcyl.es>
2022-12-30 15:30:05 +0000

    1

--- PRACTICAS/PR15_LuciaMateos/index.php
+++ PRACTICAS/PR15_LuciaMateos/index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="estilo.css">
     <title>Tienda</title>
 </head>
 <body>
@@ -12,7 +14,12 @@
     require('Conexion/conexionBD.php');
     ?>
     <header>
-
+        <div class="logo">
+            <img src="imagen/logo.png" alt="logo" class="icono_logo">
+        </div>
+        <nav class="botones">
+            <i class="fa-solid fa-cart-arrow-down"><a href="#"> Carrito</a></i>
+        </nav>
     </header>
     
 
