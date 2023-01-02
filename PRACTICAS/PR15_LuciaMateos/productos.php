437449d2758a2bd18b3ad761c957b12b97c24b5f
Lucia <lucia.matest@educa.jcyl.es>
2023-01-02 12:01:06 +0000

    1

--- PRACTICAS/PR15_LuciaMateos/productos.php
+++ PRACTICAS/PR15_LuciaMateos/productos.php
@@ -10,27 +10,57 @@
 </head>
 <body>
     <?php
-    require('Funciones/funcionesBD.php');
-    require('Conexion/conexionBD.php');
+        require('Funciones/funcionesBD.php');
+        require('Conexion/conexionBD.php');
 
-    // try {
-    //     $conexion = new PDO('mysql:host='.$_SERVER['SERVER_ADDR'].';dbname='.BBDD, USER, PASS);
-    //     $sql = 'select * from productos';
-    //     $resultado=$conexion->query($sql);
+        $opcion=false;
+        try {
+            $conexion = new PDO('mysql:host='.$_SERVER['SERVER_ADDR'].';dbname='.BBDD, USER, PASS);
+            $sql = 'select * from productos';
+            $resultado=$conexion->query($sql);
+            $array_productos=array();
+            while ($row = $resultado->fetch(PDO::FETCH_ASSOC)) {
+                array_push($array_productos,$row);
+            }
 
-    // } catch (Exception $ex) {
-    //     if ($ex->getCode() == 2002) {
-    //         echo '<span style="color:brown"> Fallo de conexión </span>';
-    //     }
-    //     if ($ex->getCode() == 1049) {
-    //         $conexion2 = new PDO('mysql:host='.$_SERVER['SERVER_ADDR'].';dbname='.BBDD, USER, PASS);
-    //         $script = usarBBDD();
-    //         $conexion2->query($script);        
-    //     }
-    //     if ($ex->getCode() == 1045) {
-    //         echo '<span style="color:brown"> Datos incorrectos </span>';
-    //     }
-    // }
+        } catch (Exception $ex) {
+            if ($ex->getCode() == 2002) {
+                echo '<span style="color:brown"> Fallo de conexión </span>';
+            }
+            if ($ex->getCode() == 1049) {
+                $opcion=true;
+                $conexion2 = new PDO('mysql:host='.$_SERVER['SERVER_ADDR'].';dbname='.BBDD, USER, PASS);
+                $script = usarBBDD();
+                $conexion2->exec($script);        
+            }
+            if ($ex->getCode() == 1045) {
+                echo '<span style="color:brown"> Datos incorrectos </span>';
+            }
+        }
+    ?>
+    <?
+        if ($opcion) {
+            try {
+                $conexion = new PDO('mysql:host='.$_SERVER['SERVER_ADDR'].';dbname='.BBDD, USER, PASS);
+                $sql = 'select * from productos';
+                $resultado=$conexion->query($sql);
+                $array_productos=array();
+                while ($row = $resultado->fetch(PDO::FETCH_ASSOC)) {
+                    array_push($array_productos,$row);
+                }
+    
+            } catch (Exception $ex) {
+                if ($ex->getCode() == 2002) {
+                    echo '<span style="color:brown"> Fallo de conexión </span>';
+                }
+                if ($ex->getCode() == 1049) {
+                    echo '<span style="color:brown"> Base de datos desconocida </span>';       
+                }
+                if ($ex->getCode() == 1045) {
+                    echo '<span style="color:brown"> Datos incorrectos </span>';
+                }
+            }
+        }
     ?>
     <header>
         <div class="logo">
@@ -54,7 +84,31 @@
         </form>
     </nav>
     <main>
-        
+        <section>
+            <?
+                foreach ($array_productos as $key) {
+                    # code...
+                }
+            ?>
+            <article>
+                <img class="ordenador" src="multimedia/ordenador1.jpg">
+                <h3>CHUWI HeroBook Pro</h3>
+                <p>Portátil equipado con DDR4 de 8GB hace que el procesamiento multitarea sea más eficiente, SSD 256GB tiene una alta velocidad de lectura y escritura,...</p>
+                <a href="#">Leer más</a>
+            </article>
+            <article>
+                <img class="ordenador" src="multimedia/ordenador2.jpg">
+                <h3>PC All in One HP Pavilion</h3>
+                <p>Este PC All-in-One contiene un 15% de plásticos reciclados posconsumo y lo equivalente a tres botellas de plástico destinado a acabar en los océanos²...</p>
+                <a href="#">Leer más</a>
+            </article>
+            <article>
+                <img class="ordenador" src="multimedia/ordenador3.jpg">
+                <h3 class="rojo">ASUS Flip C433TA</h3>
+                <p>El impresionante ASUS Chromebook Flip C433 hace gala de un estilo contemporáneo, dimensiones compactas y una selección de componentes pensada para que hagas lo que te propongas sin complicaciones...</p>
+                <a href="#">Leer más</a>
+            </article>
+        </section>
     </main>
     <footer>
         <div class="politicas">
