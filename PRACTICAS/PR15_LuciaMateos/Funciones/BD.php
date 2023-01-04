34f1b6d83e0bced710ab9347bae851073b07c2ba
Lucia <lucia.matest@educa.jcyl.es>
2023-01-04 15:39:32 +0000

    1

--- PRACTICAS/PR15_LuciaMateos/Funciones/BD.php
+++ PRACTICAS/PR15_LuciaMateos/Funciones/BD.php
@@ -3,7 +3,7 @@ require './Conexion/conexionBD.php';
 
 function validarUser($user,$pass){
     try {
-        $conexion = new PDO('mysql:host =' .HOST. ';dbname=' .BBDD, USER, PASS);
+        $conexion = new PDO('mysql:host =' .$_SERVER['SERVER_ADDR']. ';dbname=' .BBDD, USER, PASS);
         $sql = 'select * from usuarios where usuario = ? and clave = ?';
         $sql_preparada=$conexion->prepare($sql);
         //minimo sha512
@@ -35,7 +35,7 @@ function validarUser($user,$pass){
 
 function nuevoUsuario(){
     try {
-        $conexion = new PDO('mysql:host =' .HOST. ';dbname=' .BBDD, USER, PASS);
+        $conexion = new PDO('mysql:host =' .$_SERVER['SERVER_ADDR']. ';dbname=' .BBDD, USER, PASS);
         $sql="insert into usuarios values (:usuario,:clave,:nombre,:correo,:fecha,:rol);";
         $sql_preparada=$conexion->prepare($sql);
         $array = array(":usuario"=>$_REQUEST['user'],":clave"=>sha1($_REQUEST['contraseña']),":nombre"=>$_REQUEST['nombre'],":correo"=>$_REQUEST['email'],":fecha"=>$_REQUEST['fecha'],":rol"=>$_REQUEST['rol']);
@@ -48,7 +48,7 @@ function nuevoUsuario(){
 
 function validaUser($user){
     try {
-        $conexion = new PDO('mysql:host =' .HOST. ';dbname=' .BBDD, USER, PASS);
+        $conexion = new PDO('mysql:host =' .$_SERVER['SERVER_ADDR']. ';dbname=' .BBDD, USER, PASS);
         $sql="select * from usuarios where usuario= ? ;";
         $sql_preparada=$conexion->prepare($sql);
         $array = array($user);
