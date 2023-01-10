4247de177daddbadb42e0a43922f854846d8a09c
Lucia <lucia.matest@educa.jcyl.es>
2023-01-10 08:38:58 +0000

    1

--- Tema5/cookie/tienda/funciones/funcionesCookies.php
+++ Tema5/cookie/tienda/funciones/funcionesCookies.php
@@ -1,17 +1,33 @@
 <?php
     function crearCookie($id){
         //Si no existe ninguna cookie creamos [0]
-        if (!$_COOKIE['visto']) {
+        if (!isset($_COOKIE['visto'])) {
             setcookie('visto[0]',$id);
         }else {
             //El array como máximo va a tener 3
             //Si existe en el array
             $array = $_COOKIE['visto'];
+            //si el producto ya ha sido visto
             if (in_array($id, $array)) {
                 # code...
+            } else {
+                //si ya tiene 3 y no existe en el array
+                if (count($array)==3) {
+                    unset($array[0]);
+                }
+                array_push($array,$id);
             }
+            actualizar($array);
         }
         //guardar en un array
         print_r($_COOKIE['visto']);
+    }
+
+    function actualizar($array){
+        $contador = 0;
+        foreach ($array as $id) {
+            setcookie('visto['.$contador.']',$id);
+            $contador++;
+        }
     }
 ?>
