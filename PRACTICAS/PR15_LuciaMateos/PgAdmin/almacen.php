e2012900f20e05302ec50265b60ac91e1414afd6
Lucia <lucia.matest@educa.jcyl.es>
2023-01-13 21:35:50 +0000

    1

--- PRACTICAS/PR15_LuciaMateos/PgAdmin/almacen.php
+++ PRACTICAS/PR15_LuciaMateos/PgAdmin/almacen.php
@@ -76,7 +76,8 @@
                     if (esAdmin()) {
                         echo "<td>";
                         echo '<a href="../Funciones/modificarBD.php?opcion=elimina_albaran&clave='.$row['id_albaran'].'"><i class="fa-solid fa-cart-arrow-down"></i> Eliminar</a>';
-                        echo " ";
+                        echo "</td>";
+                        echo "<td>";
                         echo '<a href="../Funciones/modificarBD.php?opcion=modifica_albaran&clave='.$row['id_albaran'].'"><i class="fa-solid fa-cart-arrow-down"></i> Modificar</a>';
                         echo "</td>";     
                     }
