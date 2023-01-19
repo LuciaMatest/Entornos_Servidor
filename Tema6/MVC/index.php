a0e80d63802ec6de11f6dd30c0ef6ca719b8ad29
Lucia <lucia.matest@educa.jcyl.es>
2023-01-19 21:05:42 +0000

    1

--- Tema6/MVC/index.php
+++ Tema6/MVC/index.php
@@ -1,6 +1,20 @@
 <?php
     require('./config/configuracion.php');
 
+    if (isset($_REQUEST['logout'])) {
+        session_destroy();
+    }
+
     session_start();
 
+    if (estaValidado() && !isset($_SESSION['pagina'])) {
+        $_SESSION['vista'] = $vistas['home'];
+    } elseif ((!estaValidado() && !isset($_SESSION['pagina'])) || isset($_REQUEST['login'])) {
+        $_SESSION['pagina'] = 'login';
+        $_SESSION['controlador'] = $controladores['login'];
+        $_SESSION['vista'] = $vistas['login'];
+    } elseif (isset($_SESSION['pagina'])) {
+        require_once($_SESSION['controlador']);
+    }
+    require_once('./vista/layout.php');
 ?>
