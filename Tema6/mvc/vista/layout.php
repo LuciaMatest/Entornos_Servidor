db2c6e32e458163c3669ba7925e68264053c5df7
Lucia <lucia.matest@educa.jcyl.es>
2023-01-19 12:34:12 +0000

    1

--- Tema6/mvc/vista/layout.php
+++ Tema6/mvc/vista/layout.php
@@ -2,15 +2,12 @@
 <html lang="es">
 
 <head>
-  <title>Vista</title>
-  <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
   <!-- Bootstrap CSS v5.2.1 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-
+  integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  <title>Vista</title>
 </head>
 
 <body>
@@ -33,6 +30,7 @@
                 echo '<input type="submit" class="btn btn-warning" name="login" value="Inicia sesion" />';
             }else{
                 echo '<h2>'.$_SESSION['user'].'</h2>';
+                echo '<input type="submit" class="btn btn-warning" name="login" value="Mi perfil" />';
                 echo '<button type="submit" class="btn btn-warning" name="logout">logout</button>';
 
             }
