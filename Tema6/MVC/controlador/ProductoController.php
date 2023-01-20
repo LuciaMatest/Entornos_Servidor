5429e298c1a70d4b4117f01553ddb8e251ebe1d8
Lucia <lucia.matest@educa.jcyl.es>
2023-01-20 13:20:39 +0000

    1

--- Tema6/MVC/controlador/ProductoController.php
+++ Tema6/MVC/controlador/ProductoController.php
@@ -1,4 +1,7 @@
 <?
+if (isset($_REQUEST['codProd'])) {
     $_SESSION['producto'] = $_REQUEST['codProd'];
+} else {
     $producto = ProductoDAO::findById($_REQUEST['codProd']);
+}
 ?>
--- Tema6/MVC/vista/homeView.php
+++ Tema6/MVC/vista/homeView.php
@@ -8,9 +8,10 @@
                 <h5 class="card-title"><?echo $producto->nombre?></h5>
                 <p class="card-text"><?echo substr($producto->descripcion,0,20);?>...</p>
                 <h5 class="card-title">Precio: <?echo $producto->precio?></h5>
-                <input type="number" name="cantidad">
-                <input type="hidden" name="codProd" value="<?echo $producto->codProd?>" >
-                <input type="submit" href="./index.php" class="btn btn-primary" value ='Ver' name='ver'>
+                <form>
+                    <input type="hidden" name="codProd" value="<?echo $producto->codProd?>" >
+                    <input type="submit" href="./index.php" class="btn btn-primary" value ='Ver' name='ver'>
+                </form>
             </div>
         </div>
     <?php
