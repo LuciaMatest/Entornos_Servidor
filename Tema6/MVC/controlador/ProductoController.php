cae24d80e9acc0ec2f678236a65205a05d7d07c4
Lucia <lucia.matest@educa.jcyl.es>
2023-01-23 09:14:29 +0000

    1

--- Tema6/MVC/controlador/ProductoController.php
+++ Tema6/MVC/controlador/ProductoController.php
@@ -1,10 +1,17 @@
 <?
-if (isset($_REQUEST['codProd'])) {
+if (isset($_REQUEST['borrar'])){
+    $_SESSION['producto'] = $_REQUEST['codProd'];
+    $producto = ProductoDAO::delete($_REQUEST['codProd']);
+    $listaProducto = ProductoDAO::findAll();
+}elseif (isset($_REQUEST['editar'])){
     $_SESSION['producto'] = $_REQUEST['codProd'];
     $producto = ProductoDAO::findById($_REQUEST['codProd']);
+    $_SESSION['vista'] =  $vistas['editarProducto'];
+}elseif (isset($_REQUEST['codProd'])) {
+    $_SESSION['producto'] = $_REQUEST['codProd'];
+    $producto = ProductoDAO::findById($_REQUEST['codProd']);
+    $_SESSION['vista'] =  $vistas['verProducto'];
 } else if (isset($_REQUEST['admProductos'])) {
     $listaProducto = ProductoDAO::findAll();
-} else {
-    $producto = ProductoDAO::findById($_REQUEST['codProd']);
-}
+} 
 ?>
--- Tema6/MVC/controlador/adminController.php
+++ Tema6/MVC/controlador/adminController.php
@@ -2,5 +2,6 @@
     if (isset($_REQUEST['admProductos'])) {
         $_SESSION['vista'] = $vistas['listaProd'];
         $_SESSION['controlador'] = $controladores['producto'];
+        require_once $_SESSION['controlador'];
     }
 ?>
