a44359eae5e58f214dbe53c61ed078593b871e2e
Lucia <lucia.matest@educa.jcyl.es>
2023-01-24 09:50:04 +0000

    1

--- PRACTICAS/PR16_LuciaMateos/config/configuracion.php
+++ PRACTICAS/PR16_LuciaMateos/config/configuracion.php
@@ -4,36 +4,36 @@
     
     //CONTROLADOR
     $controladores = array(
-        'login'=>'./controlador/loginController.php',
-        'user'=>'./controlador/UserController.php',
-        'registro'=>'./controlador/registroController.php',
         'home'=>'./controlador/homeController.php',
-        'producto'=>'./controlador/ProductoController.php',
-        'admin'=>'./controlador/adminController.php'
+        // 'login'=>'./controlador/loginController.php',
+        // 'user'=>'./controlador/UserController.php',
+        // 'registro'=>'./controlador/registroController.php',
+        // 'producto'=>'./controlador/ProductoController.php',
+        // 'admin'=>'./controlador/adminController.php'
     );
 
     //CORE
     require_once('./core/funciones.php');
-    require_once('./core/validaciones.php');
+    // require_once('./core/validaciones.php');
 
     //DAO
-    require_once('./dao/DAO.php');
-    require_once('./dao/FactoryBD.php');
-    require_once('./dao/UsuarioDAO.php');
-    require_once('./dao/ProductoDAO.php');
+    // require_once('./dao/DAO.php');
+    // require_once('./dao/FactoryBD.php');
+    // require_once('./dao/UsuarioDAO.php');
+    // require_once('./dao/ProductoDAO.php');
 
     //MODELO
-    require_once('./modelo/Usuario.php');
-    require_once('./modelo/Producto.php');
+    // require_once('./modelo/Usuario.php');
+    // require_once('./modelo/Producto.php');
 
     //VISTA
     $vistas = array(
         'home'=>'./vista/homeView.php',
-        'login'=>'./vista/loginView.php',
-        'user'=>'./vista/UserView.php',
-        'registro'=>'./vista/registroView.php',
-        'verProducto'=>'./vista/ProductoView.php',
-        'admin'=>'./vista/adminView.php',
-        'listaProd'=>'./vista/listaProductosView.php',
+        // 'login'=>'./vista/loginView.php',
+        // 'user'=>'./vista/UserView.php',
+        // 'registro'=>'./vista/registroView.php',
+        // 'verProducto'=>'./vista/ProductoView.php',
+        // 'admin'=>'./vista/adminView.php',
+        // 'listaProd'=>'./vista/listaProductosView.php',
     );
 ?>
--- PRACTICAS/PR16_LuciaMateos/vista/layout.php
+++ PRACTICAS/PR16_LuciaMateos/vista/layout.php
@@ -4,51 +4,61 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <title>Vista</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="webroot/css/estilo.css">
+    <title>Tienda</title>
 </head>
 <body>
     <header>
-        <nav class="navbar navbar-expand-sm bg-secondary navbar-dark">
-        <div class="container-fluid">
-            <div class="collapse navbar-collapse" id="mynavbar">
-            <ul class="navbar-nav me-auto">
-                <li class="nav-item">
-                <a class="nav-link" href="./index.php?home">Home</a>
-                </li>
-                <!-- <li class="nav-item">
-                <a class="nav-link" href="javascript:void(0)">Features</a>
-                </li> -->
-            </ul>
-            <form class="d-flex" action='./index.php' method='post'>
-                <?php
-                    if (!estaValidado()) {
-                        echo '<div class="container mt-3">';
-                            echo '<input class="btn btn-primary" type="submit" name="login" value="Login">&nbsp;';
-                            echo '<input class="btn btn-primary" type="submit" name="registro" value="Registro">';
-                        echo '</div>';
-                    } else {
-                        echo '<div class="container mt-3">';
-                        echo '<h2 class="text-muted">'.$_SESSION['user'].'</h2>';
-                        if (esAdmin()) {
-                            echo '<input class="btn btn-primary" type="submit" name="admin" value="Administrar">&nbsp;';
-                            
-                        }
-                            echo '<input class="btn btn-primary" type="submit" name="miperfil" value="Mi perfil">&nbsp;';
-                            echo '<input class="btn btn-primary" type="submit" name="logout" value="Logout">';
-                    }
-                        echo '</div>';
-                ?>
-            </form>
+        <div class="logo">
+            <img src="webroot/img/logo.png" alt="logo" class="icono_logo">
+        </div>
+        <div class="botones">
+        <?php
+            if (estaValidado()) {
+                echo '<i class="fa-solid fa-cart-arrow-down"></i><input class="boton" type="submit" name="carrito" value="Carrito">&nbsp;';
+                echo '<i class="fa-solid fa-pen-to-square"></i><input class="boton" type="submit" name="perfil" value="Perfil">&nbsp;';
+                echo '<i class="fa-solid fa-right-from-bracket"></i><input class="boton" type="submit" name="logout" value="Cerrar sesión">';
+            } else {
+        ?>
+            <i class="fa-solid fa-user"></i><input class="boton" type="submit" name="login" value="Iniciar Sesión">
+        <?php
+            }
+        ?>
+        </div>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Tienda</a></li>
+            <li><a href="#">Contacto</a></li>
+            <li><a href="#">Ofertas</a></li>
+            <?php
+            if (esAdmin() || esModerador()) {
+                echo '<li><a href="#">Almacén</a></li>';
+                echo '<li><a href="#">Albarán</a></li>';
+                echo '<li><a href="#">Ventas</a></li>';
+            }
+            ?>
+        </ul>
+    </nav>
+    <main class="principal">
+        <div>
+            <div class="rectangulo">
+                <h2>Tu Tienda de Productos de  Belleza y Bienestar</h2>
+            </div>
+            <div class="imagenes">
+                <img src="webroot/img/inicio.jpg" alt="img1" class="img1">
+                <img src="webroot/img/inicio2.jpg" alt="img2" class="img2">
             </div>
         </div>
-        </nav>
-    </header>
-    <main>
-        <?php
-            require_once $_SESSION['vista'];
-        ?>
+        
     </main>
+    <footer>
+        <div class="politicas">
+            <a href="#">Politica de Cookies</a>
+            <a href="#">Politica de Privacidad</a>
+        </div>
+    </footer>
 </body>
 </html>
