3c62deb36171f85a6493e93ef355dca25463173c
Lucia <lucia.matest@educa.jcyl.es>
2023-01-24 07:56:58 +0000

    1

--- Tema6/MVC/index.php
+++ Tema6/MVC/index.php
@@ -13,11 +13,13 @@
         header(('Location: index.php'));
         exit;
     } else {
+        //si no tiene una vista home
         if (!isset($_SESSION['pagina'])) {
             $_SESSION['controlador'] = $controladores['home'];
             $_SESSION['pagina'] = 'home';
             $_SESSION['vista'] = $vistas['home'];
             require_once($_SESSION['controlador']);
+        //si ha pulsado login
         } elseif (isset($_REQUEST['login'])) {
             $_SESSION['pagina'] = 'login';
             $_SESSION['controlador'] = $controladores['login'];
