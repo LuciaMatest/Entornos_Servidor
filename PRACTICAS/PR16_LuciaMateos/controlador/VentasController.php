35abf1c7734c4945a2f119740f7c4395635b8592
Lucia <lucia.matest@educa.jcyl.es>
2023-02-01 23:56:53 +0000

    1

--- PRACTICAS/PR16_LuciaMateos/controlador/VentasController.php
+++ PRACTICAS/PR16_LuciaMateos/controlador/VentasController.php
@@ -1,9 +1,10 @@
 <?
     if (isset($_REQUEST['ventas'])) {
         $ventas=VentasDAO::findAll();
-    }elseif (isset($_REQUEST['eliminar'])) {
+    } elseif (isset($_REQUEST['editar'])) {
+        VentasDAO::findById($_REQUEST['id_ventas']);
+    } elseif (isset($_REQUEST['eliminar'])) {
         VentasDAO::delete($_REQUEST['id_ventas']);
     }else{
         $ventas=VentasDAO::findAll();
     }
-?>
