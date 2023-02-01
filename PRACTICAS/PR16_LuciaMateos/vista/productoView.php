5ce8472a009fdd68177b0eb1eaa26dc749cebcba
Lucia <lucia.matest@educa.jcyl.es>
2023-02-01 21:47:18 +0000

    1

--- PRACTICAS/PR16_LuciaMateos/vista/productoView.php
+++ PRACTICAS/PR16_LuciaMateos/vista/productoView.php
@@ -8,14 +8,18 @@
                     </div>
                     <div class="col-12 col-sm-12 col-md-8 col-lg-8 p-4 d-flex flex-column position-static">
                         <h3 class="fw-bold my-1 w-100 mb-1" style="color: #303030;font-size: 18px; text-transform: uppercase;line-height: 30px;"><? echo $producto->nombre ?></h3>
-                        <p><? echo $producto->descripcion ?></p>
+                        <p><? echo '<b>Cod:' . $producto->cod_producto. '</b> ' .$producto->descripcion ?></p>
                         <p class="precio py-1" style="color: #444;font-size: 45px;"><b><? echo $producto->precio ?> €</b></p>
                         <form action="./index.php" method="post">
+                            <div class="input-group w-50">
+                                <span class="input-group-text" style="background-color: #be901b;">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16" style="color: white;">
+                                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                                    </svg>
+                                </span>
+                                <input type="submit" value="Comprar" name="comprar" class="botonG" style="border-top-right-radius: 5px; border-bottom-right-radius: 5px;">
+                            </div>
                             <input type="hidden" name="cod_producto" value="<? echo $producto->cod_producto ?>">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
-                                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                            </svg>
-                            <input type="submit" value="Comprar" name="comprar" class="boton">
                         </form>
                     </div>
                 </div>
