b24c9dcb5c0b1d2306fa7953cc49db4f8bbbf7b6
Lucia <lucia.matest@educa.jcyl.es>
2023-02-01 20:26:09 +0000

    1

--- PRACTICAS/PR16_LuciaMateos/vista/layout.php
+++ PRACTICAS/PR16_LuciaMateos/vista/layout.php
@@ -47,11 +47,11 @@
                         echo '</div>';
                     } else {
                     ?>
-                        <div class="p-0 p-sm-0 p-md-2 d-flex aling">
+                        <div class="p-0 p-sm-0 p-md-2 d-flex justify-content-center align-items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
                                 <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
                             </svg>
-                            <input type="submit" value="Iniciar sesión" name="login" class="botonG px-1 d-none d-sm-none d-md-block">
+                            <input type="submit" value="Iniciar sesión" name="login" class="boton px-1 d-none d-sm-none d-md-block">
                         </div>
                     <?php
                     }
