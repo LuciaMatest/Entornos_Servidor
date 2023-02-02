6c390473d014a72126094eaa8878f42d077cf1ef
Lucia <lucia.matest@educa.jcyl.es>
2023-02-02 17:06:37 +0000

    1

--- PRACTICAS/PR16_LuciaMateos/controlador/VentasController.php
+++ PRACTICAS/PR16_LuciaMateos/controlador/VentasController.php
@@ -6,9 +6,10 @@ if (isset($_REQUEST['ventas'])) {
     $ventas = VentasDAO::delete($_REQUEST['id_ventas']);
     $ventas = VentasDAO::findAll();
 } elseif (isset($_REQUEST['comprar'])) {
-    $producto = ProductoDAO::findById($_SESSION['producto']);
-    
-    VentasDAO::insert($ventas);
+    $producto = ProductoDAO::findById($_SESSION['cod_producto']);
+    $producto->stock = $_REQUEST['stock'] - $_REQUEST['cantidad'];
+    $venta = new Ventas(null, $_SESSION['user'], date('Y-m-d'), $_REQUEST['cod_producto'], $_SESSION['cantidad'], (floatval($_REQUEST['precio'])) * (floatval($_REQUEST['cantidad'])));
+    VentasDAO::insert($venta);
     ProductoDAO::update($producto);
 } else {
     $ventas = VentasDAO::findAll();
@@ -17,7 +18,5 @@ if (isset($_REQUEST['ventas'])) {
 
 
 
-$nuevo_stock = $_REQUEST['stock'] - $_REQUEST['cantidad'];
-
-
-array($_SESSION['user'], date('Y-m-d'), $_REQUEST['cod_producto'], $_REQUEST['cantidad'], floatval($_REQUEST['precio']) * (floatval($_REQUEST['cantidad'])));
+// $nuevo_stock = $_REQUEST['stock'] - $_REQUEST['cantidad'];
+// array($_SESSION['user'], date('Y-m-d'), $_REQUEST['cod_producto'], $_REQUEST['cantidad'], floatval($_REQUEST['precio']) * (floatval($_REQUEST['cantidad'])));
