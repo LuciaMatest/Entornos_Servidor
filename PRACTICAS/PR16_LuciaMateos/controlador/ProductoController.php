9098a8fa83d06a41167bdc9fc225a78285784048
Lucia <lucia.matest@educa.jcyl.es>
2023-02-02 17:10:05 +0000

    1

--- PRACTICAS/PR16_LuciaMateos/controlador/ProductoController.php
+++ PRACTICAS/PR16_LuciaMateos/controlador/ProductoController.php
@@ -5,6 +5,7 @@ if (isset($_REQUEST['cod_producto'])) {
     $_SESSION['vista'] =  $vistas['verProducto'];
 } elseif (isset($_REQUEST['comprar'])) {
     $_SESSION['controlador'] = $controladores['ventas'];
-    $_SESSION['pagina'] = 'Home';
+    $_SESSION['pagina'] = 'Ventas';
+    $_SESSION['vista'] = $vistas['ventas'];
     require($_SESSION['controlador']);
 }
