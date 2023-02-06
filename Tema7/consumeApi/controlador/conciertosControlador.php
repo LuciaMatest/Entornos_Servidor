6e2b6e57a3344621b06154fde44b6b674e096a9e
Lucia <lucia.matest@educa.jcyl.es>
2023-02-06 08:20:22 +0000

    1

--- Tema7/consumeApi/controlador/conciertosControlador.php
+++ Tema7/consumeApi/controlador/conciertosControlador.php
@@ -1,18 +1,17 @@
 <?
-    require_once '../peticiones/curl.php';
+require_once '../peticiones/curl.php';
 
-    if (isset($_REQUEST['accion'])) {
-       if ($_REQUEST['accion']=='listar') {
-        $lista=get();
-        $lista = json_decode($lista, true);
-        require '../vista/listar.php';
-       }
-       if ($_REQUEST['accion']=='guardar') {
-          post($_REQUEST['grupo'],$_REQUEST['fecha'],$_REQUEST['precio'],$_REQUEST['lugar']);
-          $lista=get();
-          $lista = json_decode($lista, true);
-          require '../vista/listar.php';
-       } else {
-         require '../vista/formInsert.html';
-       }
-    }
+if (isset($_REQUEST['accion'])) {
+   if ($_REQUEST['accion'] == 'listar') {
+      $lista = get();
+      $lista = json_decode($lista, true);
+      require '../vista/listar.php';
+   } elseif ($_REQUEST['accion'] == 'guardar') {
+      post($_REQUEST['grupo'], $_REQUEST['fecha'], $_REQUEST['precio'], $_REQUEST['lugar']);
+      $lista = get();
+      $lista = json_decode($lista, true);
+      require '../vista/listar.php';
+   } else {
+      require '../vista/formInsert.html';
+   }
+}
