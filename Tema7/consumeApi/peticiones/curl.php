80f6a12bb5a2de85c9cf3c4235764909103ad909
Lucia <lucia.matest@educa.jcyl.es>
2023-02-06 08:02:34 +0000

    1

--- Tema7/consumeApi/peticiones/curl.php
+++ Tema7/consumeApi/peticiones/curl.php
@@ -1,14 +1,13 @@
 <?
     function get(){
-        $con= curl_init();
-        $url= 'http://192.168.2.204/Entornos_Servidor/Tema7/api/conciertos.php';
-        curl_setopt($con, CURLOPT_URL, $url);
-        //curl_setopt($con, CURLOPT_CUSTOMREQUEST, "GET");
-        curl_setopt($con, CURLOPT_RETURNTRANSFER, true);
+        $conexion= curl_init();
+        $url= 'http://192.168.2.205/ServidorClase/Tema7/api/conciertos.php';
+        curl_setopt($conexion, CURLOPT_URL, $url);
+        curl_setopt($conexion, CURLOPT_RETURNTRANSFER, true);
 
-        $resultado=curl_exec($con);
-
-        return json_decode($resultado);
+        $resultado=curl_exec($conexion);
+        curl_close($conexion);
+        return $resultado;
     }
 
     function post($grupo,$fecha,$precio,$lugar){
@@ -18,14 +17,14 @@
             "precio":"'.$precio.'" ,
             "lugar":"'.$lugar.'" 
         }';
-        $con= curl_init();
-        $url= 'http://192.168.2.204/Entornos_Servidor/Tema7/api/conciertos.php/conciertos';
-        curl_setopt($con, CURLOPT_URL, $url);
-        curl_setopt($con, CURLOPT_HTTPHEADER, 'Content-Type:application/json', array('Content-Type:application/json'));
-        curl_setopt($con, CURLOPT_POST, 1);
-        curl_setopt($con,CURLOPT_POSTFIELDS, $json);
-        $resultado=curl_exec($con);
-        curl_close($con);
+        $conexion= curl_init();
+        $url= 'http://192.168.2.205/ServidorClase/Tema7/api/conciertos.php/conciertos';
+        curl_setopt($conexion, CURLOPT_URL, $url);
+        curl_setopt($conexion, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        curl_setopt($conexion, CURLOPT_POST, 1);
+        curl_setopt($conexion,CURLOPT_POSTFIELDS, $json);
+        $resultado=curl_exec($conexion);
+        curl_close($conexion);
 
 
     }
