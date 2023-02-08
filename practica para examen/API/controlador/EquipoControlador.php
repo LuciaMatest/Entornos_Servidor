e6b6864931c62adc331fbb7eccff5ada1f38edfb
Lucia <lucia.matest@educa.jcyl.es>
2023-02-08 19:04:37 +0000

    1

--- practica para examen/API/controlador/EquipoControlador.php
+++ practica para examen/API/controlador/EquipoControlador.php
@@ -25,14 +25,102 @@ class EquipoControlador extends ControladorPadre
 
     public function buscar()
     {
+        $parametros = $this->parametros();
+        $recurso = self::recurso();
+        if (count($recurso) == 2) {
+            if (!$parametros) {
+                //Listar 
+                $lista = EquipoDAO::findAll();
+                $data = json_encode($lista);
+                self::respuesta(
+                    $data,
+                    array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+                );
+            } else {
+                // if ((isset($_GET['fecha'])) && (isset($_GET['ordenFecha']))) {
+                //     $concierto = ConciertoDAO::findByFechaOrden($_GET['fecha'], $_GET['ordenFecha']);
+                //     $data = json_encode($concierto);
+                //     self::respuesta(
+                //         $data,
+                //         array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+                //     );
+                // } elseif (isset($_GET['fecha'])) {
+                //     $concierto = ConciertoDAO::findByFecha($_GET['fecha']);
+                //     $data = json_encode($concierto);
+                //     self::respuesta(
+                //         $data,
+                //         array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+                //     );
+                // } elseif (isset($_GET['ordenFecha'])) {
+                //     if (($_GET['ordenFecha'] != 'ASC') && ($_GET['ordenFecha'] != 'DESC')) {
+                //         self::respuesta(
+                //             '',
+                //             array('HTTP/1.1 400 El filtro de fecha debe ser ASC o DESC')
+                //         );
+                //     } else {
+                //         $concierto = ConciertoDAO::findOrderByFecha($_GET['ordenFecha']);
+                //         $data = json_encode($concierto);
+                //         self::respuesta(
+                //             $data,
+                //             array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+                //         );
+                //     }
+                // } else {
+                //     self::respuesta('', array('HTTP/1.1 400 No se ha utilizado un filtro correcto'));
+                // }
+            }
+        } elseif (count($recurso) == 3) {
+            $equipo = EquipoDAO::findById($recurso[2]);
+            $data = json_encode($equipo);
+            self::respuesta(
+                $data,
+                array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+            );
+        }
     }
 
     public function insertar()
     {
+        $body = file_get_contents('php://input');
+        $dato = json_decode($body, true);
+        //propiedades
+        if (isset($dato['idEquipo']) && isset($dato['nombreEquipo']) && isset($dato['localidadEquipo'])) {
+            $equipo = new Equipo($dato['idEquipo'], $dato['nombreEquipo'], $dato['localidadEquipo']);
+            if (EquipoDAO::update($equipo)) {
+                self::respuesta(
+                    '',
+                    array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+                );
+            };
+        } else {
+            self::respuesta('', array('HTTP/1.1 400 No se ha insertado correctamente'));
+        }
     }
 
     public function modificar()
     {
+        $recurso = self::recurso();
+        if (count($recurso) == 3) {
+            $body = file_get_contents('php://input');
+            $dato = json_decode($body, true);
+            if (isset($dato['idEquipo']) && isset($dato['nombreEquipo']) && isset($dato['localidadEquipo'])) {
+                $equipo = new Equipo($dato['idEquipo'], $dato['nombreEquipo'], $dato['localidadEquipo']);
+                $equipo->idEquipo = $recurso[2];
+                if (EquipoDAO::update($equipo)) {
+                    self::respuesta(
+                        '',
+                        array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+                    );
+                } else {
+                    self::respuesta(
+                        '',
+                        array('Content-Type: application/json', 'HTTP/1.1 200 Ya no existe, no se ha modificado')
+                    );
+                }
+            }
+        } else {
+            self::respuesta('', array('HTTP/1.1 400 El recurso esta mal formado /equipo/{id}'));
+        }
     }
 
     public function borrar()
