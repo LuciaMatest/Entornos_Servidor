40146702aa647ab8d5991c751f7c1104864fb7fe
Lucia <lucia.matest@educa.jcyl.es>
2023-02-09 10:49:21 +0000

    1

--- Examen_Segunda_Lucia/Ejercicio1/controller/apuestaController.php
+++ Examen_Segunda_Lucia/Ejercicio1/controller/apuestaController.php
@@ -1,7 +1,12 @@
 <?
 if(isset($_REQUEST['modificar'])){
     $apuesta = ApuestaDAO::findById($_REQUEST['id']);
-    $apuesta->
+    $apuesta->n1 = $_REQUEST['n1'];
+    $apuesta->n2 = $_REQUEST['n2'];
+    $apuesta->n3 = $_REQUEST['n3'];
+    $apuesta->n4 = $_REQUEST['n4'];
+    $apuesta->n5 = $_REQUEST['n5'];
+    $apuesta = ApuestaDAO::update($apuesta);
     $_SESSION['vista'] = $vistas['apuesta'];
     $_SESSION['controlador'] = $controladores['apuesta'];
     $apuesta = ApuestaDAO::findAll();
--- Examen_Segunda_Lucia/Ejercicio1/core/funciones.php
+++ Examen_Segunda_Lucia/Ejercicio1/core/funciones.php
@@ -1,68 +1,36 @@
 <?
-    function estaValidado(){
-        if (isset($_SESSION['validado'])){
+function estaValidado()
+{
+    if (isset($_SESSION['validado'])) {
+        return true;
+    }
+    return false;
+}
+
+function vacio($dato)
+{
+    if (empty($_REQUEST[$dato])) {
+        return true;
+    } else {
+        return false;
+    }
+}
+//----------------------------------------------
+function esAdmin()
+{
+    if (isset($_SESSION['perfil'])) {
+        if ($_SESSION['perfil'] == 'admin') {
             return true;
         }
         return false;
     }
-    
-    function vacio($dato){
-        if (empty($_REQUEST[$dato])) {
-            return true;
-        }else {
-            return false;
-        }
-    }
-    //----------------------------------------------
-    function esAdmin(){
-        if(isset($_SESSION['perfil'])){
-            if ($_SESSION['perfil']=='admin'){
-                return true;
-            }
-            return false;
-        }
-    }
-    function esUsuario(){
-        if(isset($_SESSION['perfil'])){
-            if ($_SESSION['perfil']=='user'){
-                return true;
-            }
-            return false;
-        }
-    }
-    //------------REGeX-----------------
-function patronPass(){
-    $patron='/(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,}$/';
-    if (preg_match($patron,$_REQUEST['pass'])) {
-        return true;
-    }
-    return false;
 }
-
-
-function patronMail(){
-    $patron='/^.{1,}@.{1,}\..{2,}$/';
-    if (preg_match($patron,$_REQUEST['email'])==1){
-        return true;
-    }
-    return false;
-}
-
-function patronFecha(){
-    $patron='/^\d{4}([\-])(0?[1-9]|1[1-2])\1(3[01]|[12][0-9]|0?[1-9])$/';
-    if(preg_match($patron,$_REQUEST['fecha'])==1){
-        $cortado=explode('-',$_REQUEST['fecha']);
-        if(checkdate($cortado[1],$cortado[2],$cortado[0])){
+function esUsuario()
+{
+    if (isset($_SESSION['perfil'])) {
+        if ($_SESSION['perfil'] == 'user') {
             return true;
         }
+        return false;
     }
-    return false;
-}
-
-function patronFoto(){
-    $patron='/^[^.]+\.(jpg|png|bmp)$/';
-    if (preg_match($patron,$_FILES['url']['name'])){
-        return true;
-    }
-    return false;
 }
