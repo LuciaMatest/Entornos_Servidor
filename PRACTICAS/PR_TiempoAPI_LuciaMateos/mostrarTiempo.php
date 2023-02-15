6cb877dba4fe3dd57e9afb5d8a6d91a872c47a25
Lucia <lucia.matest@educa.jcyl.es>
2023-02-15 21:28:33 +0000

    1

--- PRACTICAS/PR_TiempoAPI_LuciaMateos/mostrarTiempo.php
+++ PRACTICAS/PR_TiempoAPI_LuciaMateos/mostrarTiempo.php
@@ -1,4 +1,9 @@
 <?
+function centigradosAFahrenheit($temperatura)
+{
+    return $temperatura * 9 / 5 + 32;
+}
+
 $conexion = curl_init();
 $url = 'http://dataservice.accuweather.com/locations/v1/cities/search?apikey=4epgPXZUKS8zBKAg2K3ozQx6O5vOAzOl&q=' . $_REQUEST['ciudad'] . '%20Castilla%20y%20Le%C3%B3n&language=es';
 curl_setopt($conexion, CURLOPT_URL, $url);
@@ -7,7 +12,7 @@ curl_setopt($conexion, CURLOPT_RETURNTRANSFER, true);
 $resultado1 = curl_exec($conexion);
 $ciudad = json_decode($resultado1, true);
 
-$url2 = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/'.$ciudad[0]['Key'].'?apikey=4epgPXZUKS8zBKAg2K3ozQx6O5vOAzOl&language=es';
+$url2 = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/' . $ciudad[0]['Key'] . '?apikey=4epgPXZUKS8zBKAg2K3ozQx6O5vOAzOl&language=es';
 curl_setopt($conexion, CURLOPT_URL, $url2);
 curl_setopt($conexion, CURLOPT_RETURNTRANSFER, true);
 
@@ -44,32 +49,39 @@ curl_close($conexion);
         <table class="table table-success table-striped text-center mt-4">
             <thead>
                 <tr>
+                    <th scope="col">#</th>
                     <th scope="col">Lunes</th>
                     <th scope="col">Martes</th>
                     <th scope="col">Miercoles</th>
                     <th scope="col">Jueves</th>
                     <th scope="col">Viernes</th>
-                    <th scope="col">Sábado</th>
-                    <th scope="col">Domingo</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td>Máxima temperatura</td>
+                    <td><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-thermometer-sun" viewBox="0 0 16 16">
+                            <path d="M5 12.5a1.5 1.5 0 1 1-2-1.415V2.5a.5.5 0 0 1 1 0v8.585A1.5 1.5 0 0 1 5 12.5z" />
+                            <path d="M1 2.5a2.5 2.5 0 0 1 5 0v7.55a3.5 3.5 0 1 1-5 0V2.5zM3.5 1A1.5 1.5 0 0 0 2 2.5v7.987l-.167.15a2.5 2.5 0 1 0 3.333 0L5 10.486V2.5A1.5 1.5 0 0 0 3.5 1zm5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0v-1a.5.5 0 0 1 .5-.5zm4.243 1.757a.5.5 0 0 1 0 .707l-.707.708a.5.5 0 1 1-.708-.708l.708-.707a.5.5 0 0 1 .707 0zM8 5.5a.5.5 0 0 1 .5-.5 3 3 0 1 1 0 6 .5.5 0 0 1 0-1 2 2 0 0 0 0-4 .5.5 0 0 1-.5-.5zM12.5 8a.5.5 0 0 1 .5-.5h1a.5.5 0 1 1 0 1h-1a.5.5 0 0 1-.5-.5zm-1.172 2.828a.5.5 0 0 1 .708 0l.707.708a.5.5 0 0 1-.707.707l-.708-.707a.5.5 0 0 1 0-.708zM8.5 12a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0v-1a.5.5 0 0 1 .5-.5z" />
+                        </svg></td>
                     <? foreach ($tiempo['DailyForecasts'] as $evento) {
-                        $temperatura = $evento['Temperature']['Maximum']['Value']; ?>
-                        <td><? $temperatura ?></td>
-                    <? }; ?>
+                        $temperatura = $evento['Temperature']['Maximum']['Value'];
+                        echo "<td>" . centigradosAFahrenheit($temperatura) . "ºC</td>";
+                    }; ?>
                 </tr>
                 <tr>
-                    <td>Mínima temperatura</td>
+                    <td><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-thermometer-snow" viewBox="0 0 16 16">
+                            <path d="M5 12.5a1.5 1.5 0 1 1-2-1.415V9.5a.5.5 0 0 1 1 0v1.585A1.5 1.5 0 0 1 5 12.5z" />
+                            <path d="M1 2.5a2.5 2.5 0 0 1 5 0v7.55a3.5 3.5 0 1 1-5 0V2.5zM3.5 1A1.5 1.5 0 0 0 2 2.5v7.987l-.167.15a2.5 2.5 0 1 0 3.333 0L5 10.486V2.5A1.5 1.5 0 0 0 3.5 1zm5 1a.5.5 0 0 1 .5.5v1.293l.646-.647a.5.5 0 0 1 .708.708L9 5.207v1.927l1.669-.963.495-1.85a.5.5 0 1 1 .966.26l-.237.882 1.12-.646a.5.5 0 0 1 .5.866l-1.12.646.884.237a.5.5 0 1 1-.26.966l-1.848-.495L9.5 8l1.669.963 1.849-.495a.5.5 0 1 1 .258.966l-.883.237 1.12.646a.5.5 0 0 1-.5.866l-1.12-.646.237.883a.5.5 0 1 1-.966.258L10.67 9.83 9 8.866v1.927l1.354 1.353a.5.5 0 0 1-.708.708L9 12.207V13.5a.5.5 0 0 1-1 0v-11a.5.5 0 0 1 .5-.5z" />
+                        </svg></td>
                     <? foreach ($tiempo['DailyForecasts'] as $evento) {
-                        $temperatura = $evento['Temperature']['Minimum']['Value']; ?>
-                        <td><? $temperatura ?></td>
-                    <? }; ?>
+                        $temperatura = $evento['Temperature']['Minimum']['Value'];
+                        echo "<td>" . centigradosAFahrenheit($temperatura) . "ºC</td>";
+                    }; ?>
                 </tr>
                 <tr>
-                    <td>Precipitaciones Día</td>
+                    <td><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sun" viewBox="0 0 16 16">
+                            <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
+                        </svg></td>
                     <? foreach ($tiempo['DailyForecasts'] as $evento) {
                         $precipitaciones = $evento['Day']['HasPrecipitation'];
                         if ($precipitaciones == null) {
@@ -80,7 +92,9 @@ curl_close($conexion);
                     }; ?>
                 </tr>
                 <tr>
-                    <td>Precipitaciones Noche</td>
+                    <td><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon" viewBox="0 0 16 16">
+                            <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z" />
+                        </svg></td>
                     <? foreach ($tiempo['DailyForecasts'] as $evento) {
                         $precipitaciones = $evento['Night']['HasPrecipitation'];
                         if ($precipitaciones == null) {
