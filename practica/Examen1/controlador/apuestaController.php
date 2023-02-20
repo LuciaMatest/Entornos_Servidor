fe76854b25d989d0b52b2ce0ff5a7b394b4f7240
Lucia <lucia.matest@educa.jcyl.es>
2023-02-20 12:40:22 +0000

    1

--- practica/Examen1/controlador/apuestaController.php
+++ practica/Examen1/controlador/apuestaController.php
@@ -1,23 +1,21 @@
 <?
-if (isset($_REQUEST['modificar'])) {
-    if (selecciona('check')) {
-        $arrayCheck = $_REQUEST['check'];
-        $arrayDatos = array($arrayCheck[0], $arrayCheck[1], $arrayCheck[2], $arrayCheck[3], $arrayCheck[4], $_SESSION["id"]);
-        if (ApuestaDAO::update($arrayDatos)) {
-            $_SESSION['controlador'] = $controladores['apuesta'];
-            $_SESSION['vista'] = $vistas['apuesta'];
-            $_SESSION['pagina'] = 'Apuesta';
-        }
-    } else {
-        $_SESSION["error"] = "<span style='color:brown'>No has seleccionado 5 números.</span>";
-    }
-} elseif (isset($_REQUEST['insertar'])) {
+if (isset($_REQUEST['insertar'])) {
     if (selecciona('check')) {
         $arrayCheck = $_REQUEST['check'];
         $apuestasTotal = ApuestaDAO::findAll();
         $apuesta = new Apuesta(count($apuestasTotal) + 1, date('Y-m-d'), $_SESSION['iduser'], $arrayCheck[0], $arrayCheck[1], $arrayCheck[2], $arrayCheck[3], $arrayCheck[4]);
-        if ($apuesta = ApuestaDAO::insert($apuesta)) {
+        if (ApuestaDAO::insert($apuesta)) {
             $_SESSION["acierto"] = true;
+            $_SESSION["id"] = $apuesta->id;
+        }
+    } else {
+        $_SESSION["error"] = "<span style='color:brown'>No has seleccionado 5 números.</span>";
+    }
+} elseif (isset($_REQUEST['modificar'])) {
+    if (selecciona('check')) {
+        $arrayCheck = $_REQUEST['check'];
+        $arrayDatos = array($arrayCheck[0], $arrayCheck[1], $arrayCheck[2], $arrayCheck[3], $arrayCheck[4], $_SESSION["id"]);
+        if (ApuestaDAO::update($arrayDatos)) {
             $_SESSION['controlador'] = $controladores['apuesta'];
             $_SESSION['vista'] = $vistas['apuesta'];
             $_SESSION['pagina'] = 'Apuesta';
